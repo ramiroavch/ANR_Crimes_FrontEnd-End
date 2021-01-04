9cf655bc94c3ed9aa9b9b02ba4b7b919cfcbec78
Ramiro Vargas <ramiro.vargas@payco.co>
2021-01-03 22:11:05 -0400

    Refactor(Login): Crea componente reducer y agrega llamadas desde el login

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import App from './App';
 import ReactDOM from 'react-dom';
+import {createStore, combineReducers} from 'redux';
+import loginReducer from './store/reducers/login';
+import {Provider} from 'react-redux';
+
+const rootReducer= combineReducers({
+    loginReducer
+});
+const store = createStore(rootReducer,
+    window.REDUX_DEVTOOLS_EXTENSION && window.REDUX_DEVTOOLS_EXTENSION());
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
